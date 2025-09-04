1e890f829e3dbeb7334b5abfef060a40e52c37b4
giannibi <giannibi@giannibi.net>
2025-09-04 21:48:28 +0200

    Added LICENSE and README.md, updated setup.py and meta.yaml for Python 3.10

---
+++ README.md
@@ -0,0 +1,2 @@
+# Robust-and-Predictive-Control
+This repository hosts the Python projects for the Robust and Predictive Control course at DIISM - Universita' di Siena
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="rpc_diism",
     version="0.1.0",
-    description="Template library for rpc_diism",
-    author="",
+    description="Python library for the Robust and Predictive Control course at DIISM, Unisi",
+    author="Gianni Bianchini",
     packages=find_packages(),
     install_requires=[
         "control",
         "slycot"
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
 )
