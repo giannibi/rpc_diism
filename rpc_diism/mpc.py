e64e1ea800853b28636ae694d5253912aa108439
giannibi <giannibi@giannibi.net>
2025-09-07 17:03:35 +0200

    First complete version

--- rpc_diism/mpc.py
+++ rpc_diism/mpc.py
@@ -3,11 +3,12 @@ from itertools import product
 import numpy as np
 import control as ct
 import cvxpy as cp
-import warnings
 
+
+""" GENERAL FUNCTIONS """
 
 def sampled_data_controller(controller, plant_dt): 
-    '''
+    """
     Create a discrete-time system that models the behaviour 
     of a digital controller. 
     
@@ -22,9 +23,9 @@ def sampled_data_controller(controller, plant_dt):
     The plant that is connected to it must be converted to a discrete-time 
     ZOH equivalent with a sampling interval that is also `plant_dt`. A 
     controller that is a pure gain must have its `dt` specified (not None).
-    ''' 
+    """ 
     
-    # the following is used to ensure the number before '%' is a bit larger 
+    # The following is used to ensure the number before '%' is a bit larger 
     one_plus_eps = 1 + np.finfo(float).eps 
     assert np.isclose(0, controller.dt*one_plus_eps % plant_dt), \
         "plant_dt must be an integral multiple of the controller's dt"
@@ -35,7 +36,7 @@ def sampled_data_controller(controller, plant_dt):
     def updatefunction(t, x, u, params):  
         nonlocal step
 
-        # Update the controller state only if it is time to sample
+        # Update the controller state only if it is time to sample 
         if step == 0:
             x = controller._rhs(t, x, u)
         step += 1
@@ -47,28 +48,32 @@ def sampled_data_controller(controller, plant_dt):
     def outputfunction(t, x, u, params):
         nonlocal y
         
-        # Compute controller action if it is time to sample
+        # Compute controller action if it is time to sample 
         if step == 0:
             y = controller._out(t, x, u)       
         return y
 
-    # Return the controller system object
+    # Return the controller system object 
     return ct.ss(updatefunction, outputfunction, dt=plant_dt, 
                  name=controller.name, inputs=controller.input_labels, 
                  outputs=controller.output_labels, states=controller.state_labels)
 
 
+
+""" MPC RELATED FUNCTIONS """
+
 def terminal_cost_set(p):
-    '''
+    """
     Computes an ellipsoidal terminal set x^T * Qt * x <= 1 and an associated terminal cost
     x^T * P * x, on the basis of a nominal controller computed with the same cost weights as the MPC problem
     p: MPC controller parameters
-     '''
-    # Terminal controller, computed as an LQR
+     """
+    
+    # Terminal controller, computed as an LQR 
     K, P, _ = ct.dlqr(p.A, p.B, p.Q, p.R)
     K = -K
     
-    # State constraints under the nominal controller
+    # State constraints under the nominal controller 
     Z = np.vstack((p.F,p.E @ K))
     z = np.vstack((p.f, p.e))
 
@@ -76,7 +81,7 @@ def terminal_cost_set(p):
     (n, _) = p.A.shape
 
     # Compute the maximal sublevel set of the terminal cost x^T * P * x
-    # that lies within the constraints for the terminal controller
+    # that lies within the constraints for the terminal controller 
     a = cp.Variable(nz)
     gamma = cp.Variable(1)
     epsi = np.finfo(float).eps
@@ -99,8 +104,10 @@ def terminal_cost_set(p):
         return P, Qt
     
 
-def quadratic_mpc_problem(p):
-    '''
+""" BASIC MPC IMPLEMENTATION TO REGULATE THE SYSTEM TO ZERO """
+
+def quadratic_mpc_problem_basic(p):
+    """
     Generate a cvxpy problem for the quadratic MPC step
     p: Controller parameters
         A,B: Model
@@ -113,39 +120,40 @@ def quadratic_mpc_problem(p):
     Parameters:
     x0: initial state
     returns cvxpy problem object with x0 as parameter
-    '''
-    # State and input dimension
+    """
+
+    # State and input dimension 
     (nx, nu) = p.B.shape
 
-    # Define initial state as parameter  
+    # Define initial state as parameter
     x0 = cp.Parameter(nx, name='x0')
 
-    # Define state and input variables
+    # Define state and input variables 
     x = cp.Variable((nx, p.N + 1), name='x') # From k=0 to k=N
     u = cp.Variable((nu, p.N), name='u')     # From k=0 to k=N-1
 
-    # Initialize cost and constraints
+    # Initialize cost and constraints 
     cost = 0.0
     constraints = []
     
-    # Generate cost and constraints
+    # Generate cost and constraints 
     for k in range(p.N):
         
-        # Stage cost
+        # Stage cost 
         cost +=  cp.quad_form(x[:, k], p.Q)
         cost +=  cp.quad_form(u[:, k], p.R)
 
-        # Dynamics constraint
+        # Dynamics constraint 
         constraints += [x[:, k + 1] == p.A @ x[:, k] + p.B @ u[:, k]]
 
-        # State constraints
+        # State constraints 
         if p.F is not None:
             constraints += [p.F @ x[:, k] <= p.f.reshape(-1)]
         
-    # Add terminal cost
+    # Add terminal cost 
     cost +=  cp.quad_form(x[:, p.N], p.P)
     
-    # Terminal constraint: if unspecified, fall back to trivial terminal constraint
+    # Terminal constraint: if unspecified, fall back to trivial terminal constraint 
     if p.Qt is not None:
         constraints += [cp.quad_form(x[:, p.N], p.Qt) <= 1]
     else:
@@ -165,22 +173,21 @@ def quadratic_mpc_problem(p):
     return problem
 
 
-def mpc_controller_regulation(parameters, T, **kwargs):
-        '''
+def mpc_controller_basic(p, T, **kwargs):
+        """
         Create an I/O system implementing an MPC controller
-        parameters: Controller parameters
+        p: Controller parameters
             A,B: Model
             N: Horizon length
             Q,R,P: Cost weights
         T: Sampling period
-        '''
+        """
     
-        p = parameters
         nx, nu = p.B.shape
         ux = np.zeros((nu, 1))
     
         # Set up the cvxpy problem to be solved at each step
-        problem = quadratic_mpc_problem(p)
+        problem = quadratic_mpc_problem_basic(p)
       
         # State x of the MPC controller is the current optimal sequence
         # while its input u is composed by dummy inputs and current plant state
@@ -233,18 +240,92 @@ def mpc_controller_regulation(parameters, T, **kwargs):
             _update, _output, dt=T, **kwargs)
 
 
-def mpc_controller_staticref(parameters, T, **kwargs):
+
+
+""" MPC IMPLEMENTATION WITH CONSTANT REFERENCE TRACKING, NO CONSTRAINT RELAXATION """
+
+
+def quadratic_mpc_problem(p):
+    """
+    Generate a cvxpy problem for the quadratic MPC step
+    Terminal set and cost are computed internally as an LQR set/cost
+    p: Controller parameters
+        A,B: Model
+        N: Horizon length
+        Q, R: State and input weights for stage cost
+        Fx <= f: State constraints
+        Eu <= e: Input constraints
+        Xf: if == None, use the trivial zero terminal constraint
+            if == 'lqr', compute an ellipsoidal terminal set
+            and quadratic terminal cost based on lqr with weights p.Q, p.R
+    Parameters:
+    x0: initial state
+    Returns cvxpy problem object with x0 as parameter, to be solved at each MPC step
+    using current state as parameter
+    """
+    
+    # State and input dimension
+    (nx, nu) = p.B.shape
+
+    # Define initial state as parameter  
+    x0 = cp.Parameter(nx, name='x0')
+
+    # Define state and input variables
+    x = cp.Variable((nx, p.N + 1), name='x') # From k=0 to k=N
+    u = cp.Variable((nu, p.N), name='u')     # From k=0 to k=N-1
+
+    # Initialize cost and constraints
+    cost = 0.0
+    constraints = []
+    
+    # Generate cost and constraints
+    for k in range(p.N):
+        
+        # Stage cost
+        cost +=  cp.quad_form(x[:, k], p.Q)
+        cost +=  cp.quad_form(u[:, k], p.R)
+
+        # Dynamics constraint
+        constraints += [x[:, k + 1] == p.A @ x[:, k] + p.B @ u[:, k]]
+
+        # State constraints
+        if p.F is not None:
+            constraints += [p.F @ x[:, k] <= p.f.reshape(-1)]
+        
+    # Terminal cost and constraint:
+    # if unspecified, fall back to trivial terminal constraint
+    if p.Xf == 'lqr': # Ellipsoidal/lqr set/cost
+        P,Qt = terminal_cost_set(p)
+        cost += cp.quad_form(x[:, p.N], P)
+        constraints += [cp.quad_form(x[:, p.N], Qt) <= 1]
+    else:
+        constraints += [x[:, p.N] == np.zeros_like(x0) ]
+    
+    # Input constraints
+    if p.E is not None:
+        constraints += [p.E @ u <= p.e]  
+
+    # Initial state constraint
+    constraints += [x[:, 0] == x0]
+
+    # Generate cvx problem
+    problem = cp.Problem(cp.Minimize(cost), constraints)
+    
+    # Return problem object
+    return problem
+
+
+def mpc_controller(p, T, **kwargs):
         """
         Create an I/O system implementing an MPC controller
-        parameters: Controller parameters
-            ref: Reference to be tracked
+        p: Controller parameters
+            ref: Constant reference to be tracked
             A,B: Model
             N: Horizon length
             Q,R: Cost weights
         T: Sampling period
         """
-        p = parameters
-        typ = type
+
         nx, nu = p.B.shape
         no, _ = p.C.shape
 
@@ -319,14 +400,17 @@ def mpc_controller_staticref(parameters, T, **kwargs):
             _update, _output, dt=T, **kwargs)
 
 
+
+""" MPC IMPLEMENTATION WITH DYNAMIC (PIECEWISE CONSTANT) REFERENCE TRACKING AND CONSTRAINT RELAXATION """
+
 def terminal_cost_set_with_relax(p):
-    '''
+    """
     Computes an ellipsoidal terminal set x^T * Qt * x <= 1 and an associated terminal cost
     x^T * P * x, on the basis of a nominal controller computed with the same cost weights as the MPC problem
     p: MPC controller parameters
     returns terminal cost matrix P, ellipsoidal terminal set matrix Qt and a
     status variable to check if the terminal set is empty or unbounded
-     '''
+     """
     # Terminal controller, computed as an LQR
     K, P, _ = ct.dlqr(p.A, p.B, p.Q, p.R)
     K = -K
@@ -360,14 +444,14 @@ def terminal_cost_set_with_relax(p):
     problem = cp.Problem(cp.Minimize(gamma), constraints)
     problem.solve(solver='MOSEK')
     if problem.status == 'unbounded':
-        warnings.warn("WARNING: terminal set is unbounded: is your problem even constrained?",
+        print("WARNING: terminal set is unbounded: is your problem even constrained?",
                "If not, use LQR :) ")
         Qt = np.zeros([n,n])
     elif problem.status == 'optimal':
         # Terminal set is defined by x^T * Qt * x <= 1
         Qt = gamma.value * P
     elif problem.status == 'infeasible':
-        warnings.warn("WARNING: terminal set is empty: probably the origin",
+        print("WARNING: terminal set is empty: probably the origin",
                 "is in the infeasible region, is your problem overconstrained?")
         Qt = -1 * np.eye(n)
     else:
@@ -501,7 +585,7 @@ def mpc_controller_with_relax(parameters, T, **kwargs):
         # Terminal set is unbounded: Qt=0
         elif status == 'infeasible':
             # Terminal set is empty: the constraint set is probably empty
-            warnings.warn("Terminal set is empty: falling back to zero terminal", 
+            print("Terminal set is empty: falling back to zero terminal", 
                     "constraint but something is probably wrong with your",
                     "constraints, expect infeasibility")
 
@@ -528,7 +612,7 @@ def mpc_controller_with_relax(parameters, T, **kwargs):
             dummy = 0 # Do nothing       
         elif status == 'infeasible':
             # Terminal set for new reference is empty: trigger relaxation and recompute terminal set
-            warnings.warn(f"Relaxation triggered for empty terminal set at t={t}")
+            print(f"Relaxation triggered for empty terminal set at t={t}")
             p.alpharelax = p.alpharelaxe
             p.P, p.Qt, status = terminal_cost_set_with_relax(p)
             if status != 'optimal':
@@ -555,7 +639,7 @@ def mpc_controller_with_relax(parameters, T, **kwargs):
         ref = u[:no]
 
         # See if the reference changed. If so, update problem
-        if lastref != ref:
+        if not ((lastref == ref).all()):
             lastref = ref
             update_mpc_problem(ref, t)
                
@@ -572,7 +656,7 @@ def mpc_controller_with_relax(parameters, T, **kwargs):
                 raise(ValueError("Infeasible relaxed problem: giving up"))
             else:
                 # Generate and solve a relaxed problem with soft state constraints
-                warnings.warn(f"Relaxation triggered for infeasibility at t={t}")
+                print(f"Relaxation triggered for infeasibility at t={t}")
                 p.alpharelax = p.alpharelaxe
                 p.P, p.Qt, status = terminal_cost_set_with_relax(p)
                 problem = quadratic_mpc_problem_with_relax(p)
@@ -604,7 +688,7 @@ def mpc_controller_with_relax(parameters, T, **kwargs):
         ref = u[:no]
 
         # See if the reference changed. If so, update problem
-        if lastref != ref:
+        if not ((lastref == ref).all()):
             lastref = ref
             update_mpc_problem(ref, t)
                
@@ -621,7 +705,7 @@ def mpc_controller_with_relax(parameters, T, **kwargs):
                 raise(ValueError("Infeasible relaxed problem: giving up"))
             else:
                 # Generate and solve a relaxed problem with soft state constraints
-                warnings.warn(f"Relaxation triggered for infeasibility at t={t}")
+                print(f"Relaxation triggered for infeasibility at t={t}")
                 p.alpharelax = p.alpharelaxe
                 p.P, p.Qt, status = terminal_cost_set_with_relax(p)
                 problem = quadratic_mpc_problem_with_relax(p)
